0691339cb08fc6db15d27936c929fa4943ac11cf
Seed <160701034@qq.com>
2026-09-21 22:12:03 +0800

    Add function: Multi_Input

--- Charactor.cpp
+++ Charactor.cpp
@@ -1,3 +1,28 @@
-//
-// Created by cenha on 2026/9/21.
-//
+#include <iostream>
+using namespace std;
+
+/*
+ *类型自动转换规则：
+ *1.自动向精度更高、范围更大的类型转换
+ *  整型提升：char， short参与运算，先全部提升为int， 然后向更精准大范围的的类型转换
+ *  bool - char - short - int - long - long long - float - double - long double
+ *2.运算时，两边操作数会统一转换成两者中等级更高的类型，结果也是该类型
+ *  如 int + double会触发 int转 double， 结果也是 double
+ *3.int / int整数除法，不会自动变成浮点数
+ *4.有符号和无符号混合运算，有符号会转成无符号，容易出现负数变成超大正数
+ *5.赋值转换：把右边表达式的值强制转为左边变量的类型，会发生截断 / 舍入
+ *  -浮点赋给整型：比如int i; double d = 3.99; i = d;就会 double转 int，直接截断小数部分，i = 3
+ *  -大范围类型赋值给小范围：会丢失高位，可能溢出
+ *  -函数参数匹配：实参自动转换为形参类型
+ *
+ *显式强制类型转换：
+ *
+ *
+ */
+
+
+int main()
+{
+    system("pause");
+    return 0;
+}
--- IO_Test.cpp
+++ IO_Test.cpp
@@ -3,6 +3,38 @@
 #include <cstdint>
 using namespace std;
 
+//一行多输入
+#include <vector>
+#include <string>
+#include <sstream>
+#include <limits>
+
+
+void Multi_Input(vector<int>& v)
+{
+    string line; //line一会用于存储从cin得到的一整行数据
+    cout << "请输入若干数字，可使用空格分隔，按回车提交" << endl;
+    cin.ignore(); //cin.ignore()可以忽略掉前面cin << a;等剩下的一个\n, 但是只能忽略一个，如果回车前还有空格等就会失效
+    //cin.ignore(numeric_limits<streamsize>::max(), '\n'); 可以清除回车之前的所有东西
+    getline(cin, line); //getline可以实现从cin中拿到一整行的输入数据，放在line里
+    stringstream ss(line); //stringstream ss(line) 可以从line中构造输入流数据给ss， 并且可以从中分割数据
+
+    int tmp;
+    while (ss >> tmp)
+    {
+        v.push_back(tmp);
+    }
+}
+
+void Print_Vector(vector<int>& v)
+{
+    for (auto x : v)
+    {
+        cout << x << endl;
+    }
+}
+
+
 float GPA_Turn(uint16_t score)
 {
     float GPA;
@@ -10,6 +42,7 @@ float GPA_Turn(uint16_t score)
 
     return GPA;
 }
+
 
 int main()
 {
@@ -30,6 +63,11 @@ int main()
     {
         cout << "Get fucking out" << endl;
     }
+
+    vector<int> v;
+    Multi_Input(v);
+    Print_Vector(v);
+
 
     system("pause");
     return 0;
