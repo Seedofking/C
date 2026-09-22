ce1b04fb34fb6ab3ab3ce01af98c55732746255e
Seed <160701034@qq.com>
2026-09-22 20:31:04 +0800

    Solved i.ignore()

--- IO_Test.cpp
+++ IO_Test.cpp
@@ -2,19 +2,20 @@
 //表示使用 input / ouput 输入/输出数据流
 #include <cstdint>
 using namespace std;
+#include<vector>
 
 //一行多输入
-#include <vector>
 #include <string>
 #include <sstream>
 #include <limits>
-
 
 void Multi_Input(vector<int>& v)
 {
     string line; //line一会用于存储从cin得到的一整行数据
     cout << "请输入若干数字，可使用空格分隔，按回车提交" << endl;
-    cin.ignore(); //cin.ignore()可以忽略掉前面cin << a;等剩下的一个\n, 但是只能忽略一个，如果回车前还有空格等就会失效
+    //cin.ignore(numeric_limits<streamsize>::max(), '\n');这个东西的问题是，如果前面没有换行可供读取，就永远不会结束程序
+    //cin.ignore();   //这个东西的问题是，如果前面没有换行可供丢弃，就会丢弃数据
+    //cin.ignore()可以忽略掉前面cin << a;等剩下的一个\n, 但是只能忽略一个，如果回车前还有空格等就会失效
     //cin.ignore(numeric_limits<streamsize>::max(), '\n'); 可以清除回车之前的所有东西
     getline(cin, line); //getline可以实现从cin中拿到一整行的输入数据，放在line里
     stringstream ss(line); //stringstream ss(line) 可以从line中构造输入流数据给ss， 并且可以从中分割数据
@@ -25,6 +26,7 @@ void Multi_Input(vector<int>& v)
         v.push_back(tmp);
     }
 }
+
 
 void Print_Vector(vector<int>& v)
 {
